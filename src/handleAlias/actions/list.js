f7902bebf4eddf40c4edcfd0be0941d464c3aa16
Filip Krawczyk <fkrawczyk77@gmail.com>
2022-10-16 18:17:29 +0100

    Feat: alias list sorting

--- src/handleAlias/actions/list.js
+++ src/handleAlias/actions/list.js
@@ -1,29 +1,22 @@
-const sourceConfig = require("../../sourceConfig");
 const clc = require("cli-color");
+const { getAllAliases } = require("../helpers");
 
 function handleList(args) {
-  const sourcesWithAliases = sourceConfig
-    .getConfigFlat()
-    .filter((source) => source.aliases);
+  const aliases = getAllAliases().sort((a, b) =>
+    a.alias.localeCompare(b.alias)
+  );
 
-  for (const source of sourcesWithAliases) {
-    for (const alias of source.aliases) {
-      console.log(
-        `${clc.green.bold(alias.alias)} ${clc.green(
-          `(${source.name}/${alias.relativePath})`
-        )}`
-      );
-      if (args.verbose) {
-        console.log(alias.content.trim());
-        console.log();
-      }
+  for (const alias of aliases) {
+    console.log(
+      `${clc.green.bold(alias.alias)} ${clc.green(`(${alias.relativePath})`)}`
+    );
+    if (args.verbose) {
+      console.log(alias.content.trim());
+      console.log();
     }
   }
 
-  const aliasesCount = sourcesWithAliases.reduce(
-    (acc, source) => acc + source.aliases.length,
-    0
-  );
+  const aliasesCount = aliases.length;
   console.log(
     clc.blue.bold(
       `${aliasesCount} alias${aliasesCount === 1 ? "" : "es"} found`
