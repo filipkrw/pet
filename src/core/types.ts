08ad37a2fd7f441ea6ffacb21c2940013bd3da04
Filip Krawczyk <filip@algomo.com>
2023-06-12 20:35:25 +0100

    Remove plugin setup

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,11 +1,8 @@
-import { FindArgs } from "../features/notes/findNotes/parseFindArgv";
-
 export type Vault<T = unknown> = {
   relativePath: string;
   absolutePath: string;
   includePatterns?: string[];
   excludePatterns?: string[];
-  plugins?: VaultPlugin[];
   textEditor?: string;
   features?: {
     disable?: string[];
@@ -17,17 +14,6 @@ export type VaultWithSubVaults<T = unknown> = Vault<T> & {
   vaults?: VaultWithSubVaults<T>[];
 };
 
-export type VaultPlugin = {
-  command: "find";
-  flow(configs: LoadedConfigs): Promise<unknown>;
-};
-
 export type LocalConfig = {
   basePath: string;
-};
-
-export type LoadedConfigs = {
-  args: FindArgs;
-  localConfig: LocalConfig;
-  vault: VaultWithSubVaults<unknown>;
 };
