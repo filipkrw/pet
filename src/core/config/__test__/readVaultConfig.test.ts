c99b03b974c46868b61feb3755ef27242fd59b18
Filip Krawczyk <filip@algomo.com>
2023-06-13 22:22:04 +0100

    Implement readVaultConfig

--- src/core/config/__test__/readVaultConfig.test.ts
+++ src/core/config/__test__/readVaultConfig.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 import path from "path";
-import { afterEach, beforeEach, expect, test } from "vitest";
+import { afterEach, beforeEach, test } from "vitest";
 import {
   writeLocalConfig,
   writeVaultConfig,
@@ -9,18 +9,29 @@ import { getRootPath } from "../../../util/getRootPath";
 import { exec } from "../../exec";
 import { readVaultConfig } from "../readVaultConfig.js";
 
-const testBasePath = path.join(getRootPath(), ".tmp");
+const basePath = path.join(getRootPath(), ".tmp");
+const subVaultPaths = [
+  path.join(basePath, "subvault"),
+  path.join(basePath, "subvault", "super-nested-subvault"),
+  path.join(basePath, "another-subvault"),
+];
 
 beforeEach(async () => {
-  await Promise.resolve({
-    localConfig: { basePath: testBasePath },
-  })
-    .then((x) => exec(x, writeLocalConfig))
-    .then((x) => exec(x, writeVaultConfig));
+  await Promise.resolve({ localConfig: { basePath } })
+    .then((x) => exec(x, writeVaultConfig))
+    .then(() =>
+      exec({ localConfig: { basePath: subVaultPaths[0] } }, writeVaultConfig)
+    )
+    .then(() =>
+      exec({ localConfig: { basePath: subVaultPaths[1] } }, writeVaultConfig)
+    )
+    .then(() =>
+      exec({ localConfig: { basePath: subVaultPaths[2] } }, writeVaultConfig)
+    );
 });
 
 afterEach(async () => {
-  await fs.rm(testBasePath, { recursive: true });
+  await fs.rm(basePath, { recursive: true });
 });
 
 test("readVaultConfig", async () => {
@@ -29,7 +40,7 @@ test("readVaultConfig", async () => {
       name: "notes",
     },
     localConfig: {
-      basePath: testBasePath,
+      basePath: basePath,
     },
   });
   console.log(vaults);
--- src/core/config/readVaultConfig.ts
+++ src/core/config/readVaultConfig.ts
@@ -1,10 +1,22 @@
+import fg from "fast-glob";
 import { z } from "zod";
-import { LocalConfig } from "../types";
 import { FeatureMeta } from "../Feature";
+import { LocalConfig } from "../types";
+import { importConfigFile } from "./importConfig";
 
 const vaultSchema = z.object({
   relativePath: z.string(),
   absolutePath: z.string(),
+  config: z.object({
+    includePatterns: z.array(z.string()).optional(),
+    excludePatterns: z.array(z.string()).optional(),
+    features: z
+      .object({
+        enable: z.array(z.string()).optional(),
+        disable: z.array(z.string()).optional(),
+      })
+      .optional(),
+  }),
 });
 
 type Vault = z.infer<typeof vaultSchema>;
@@ -16,5 +28,49 @@ export async function readVaultConfig({
   localConfig: LocalConfig;
   feature: FeatureMeta;
 }): Promise<Vault[]> {
-  return [];
+  const configFilePaths = await fg([
+    `${localConfig.basePath}/**/.pet/config.mjs`,
+  ]);
+  const vaults: Vault[] = [];
+
+  for (const configFilePath of configFilePaths) {
+    const config = await importConfigFile(configFilePath);
+
+    const relativePath = configFilePath
+      .replace(localConfig.basePath, "")
+      .replace("/.pet/config.mjs", "")
+      .replace(/^\//, "");
+
+    const vault = vaultSchema.safeParse({
+      relativePath,
+      absolutePath: configFilePath,
+      config,
+    });
+
+    if (!vault.success || !isFeatureAllowed(vault.data, feature)) {
+      console.log(`Invalid config file: ${relativePath}`);
+      continue;
+    }
+
+    vaults.push(vault.data);
+  }
+
+  return vaults;
+}
+
+function isFeatureAllowed(vault: Vault, feature: FeatureMeta) {
+  if (!vault.config.features) {
+    return true;
+  }
+
+  if (vault.config.features.disable?.includes(feature.name)) {
+    return false;
+  }
+
+  if (
+    vault.config.features.disable?.includes("*") &&
+    !vault.config.features.enable?.includes(feature.name)
+  ) {
+    return false;
+  }
 }
