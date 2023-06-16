c1eb4199cf4229a763ae76217d98cb554f706612
Filip Krawczyk <filip@algomo.com>
2023-06-16 19:27:21 +0100

    Bust `importConfig` cache

--- src/core/config/importConfig.ts
+++ src/core/config/importConfig.ts
@@ -1,6 +1,9 @@
 import { pathToFileURL } from "url";
 
 export async function importConfigFile(path: string): Promise<unknown> {
-  const { default: config } = await import(pathToFileURL(path).toString());
+  const cacheBustingPath = `${pathToFileURL(
+    path
+  ).toString()}?update=${Date.now()}`;
+  const { default: config } = await import(cacheBustingPath);
   return config;
 }
