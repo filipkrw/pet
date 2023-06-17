ae56862bc981997941158be19f6db1409af9f988
Filip Krawczyk <filip@algomo.com>
2023-06-17 17:13:51 +0100

    Add shadcn form

--- packages/chrome-extension/src/App.tsx
+++ packages/chrome-extension/src/App.tsx
@@ -3,6 +3,15 @@ import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 import { trpc } from "./trpc";
 import { Ping } from "./Ping";
+import { BookmarkForm } from "./BookmarkForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
 export const App: React.FC = () => {
   const [queryClient] = useState(() => new QueryClient());
@@ -15,7 +24,17 @@ export const App: React.FC = () => {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <Ping />
+        <div className="w-full h-full flex justify-center items-center">
+          <Card className="w-[484px]">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <BookmarkForm />
+            </CardContent>
+          </Card>
+        </div>
       </QueryClientProvider>
     </trpc.Provider>
   );
