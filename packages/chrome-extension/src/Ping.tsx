86eb25e29e08653a8374777764e0f0280917b9a4
Filip Krawczyk <filip@algomo.com>
2023-06-17 15:55:58 +0100

    Send current tab url to pet

--- packages/chrome-extension/src/Ping.tsx
+++ packages/chrome-extension/src/Ping.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { trpc } from "./trpc";
 
 export const Ping: React.FC = () => {
-  const { data } = trpc.ping.useQuery();
-  const createBookmark = trpc.createBookmark.useMutation();
+  const [error, setError] = useState<string | null>(null);
+  const { mutate: createBookmark } = trpc.createBookmark.useMutation();
+
+  async function handleClick() {
+    const tabs = await chrome.tabs.query({
+      active: true,
+      windowId: chrome.windows.WINDOW_ID_CURRENT,
+    });
+    const activeTab = tabs[0];
+    if (!activeTab) {
+      setError("No active tab");
+      return;
+    }
+    if (!activeTab.url) {
+      setError("Active tab has no URL");
+      return;
+    }
+    createBookmark({ url: activeTab.url });
+  }
 
   return (
     <>
-      <pre>{JSON.stringify(data)}</pre>
-      <button
-        onClick={() => createBookmark.mutate({ url: "https://google.com" })}
-      >
-        Create bookmark
-      </button>
+      <button onClick={handleClick}>Create bookmark</button>
+      {error && <p>{error}</p>}
     </>
   );
 };
